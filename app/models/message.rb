90741f1e36c3618acfb09f792af115938b46f863
Emiliano Lopez <emilianolch@gmail.com>
2021-12-20 23:01:51 -0300

    validates message content is not empty

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,6 @@ class Message < ApplicationRecord
   belongs_to :user
   # Broadcast a model rendering over ActionCable after create/update/destroy.
   broadcasts_to :room 
+
+  validates :content, presence: true
 end
