75e7ed278f50e6b6d63ee8e6644d0596c795348d
Emiliano Lopez <emilianolch@gmail.com>
2021-12-20 23:52:33 -0300

    assets debug enabled

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -3,5 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   reset() {
     this.element.reset()
+
+    // Scroll page to bottom
+    // Not workings
+    console.log('hello worldsss')
+    const { scrollHeight, clientHeight, offsetHeight } = this.element
+    if (scrollHeight >= clientHeight) {
+      this.element.scrollTop = scrollHeight - clientHeight
+    }
   }
 }
