ab6b793d88bc99b931d1736652b64247d724d9c3
Emiliano Lopez <emilianolch@gmail.com>
2021-12-21 00:14:55 -0300

    scroll down on message push

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,15 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  connect() {
+    window.scrollTo(0, document.body.scrollHeight)
+  }
   reset() {
     this.element.reset()
 
     // Scroll page to bottom
-    // Not workings
-    console.log('hello worldsss')
-    const { scrollHeight, clientHeight, offsetHeight } = this.element
-    if (scrollHeight >= clientHeight) {
-      this.element.scrollTop = scrollHeight - clientHeight
-    }
+    window.scrollTo(0, document.body.scrollHeight)
   }
 }
