4a7c29256418cb176d24ff10084eaa1e25ac23c8
Emiliano Lopez <emilianolch@gmail.com>
2021-12-25 11:12:54 -0300

    disabled turbo for sign_in and delete accout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  #
+  # Devise customization
+  #
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
