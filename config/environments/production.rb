40e9d262305328158c0f7a0eb72828a303e1312b
Emiliano Lopez <emilianolch@gmail.com>
2021-12-25 12:23:11 -0300

    configured production default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_controller.default_url_options = { host: ENV['hostname'] }
+  config.action_mailer.default_url_options = { host: ENV['hostname'] }
+
 end
